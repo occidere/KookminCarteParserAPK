708fa94a3e53217db4f5d6d30a9f20153f5b14c4
S. J. Lee <sj9401h@naver.com>
2016-11-23 14:41:59 +0900

    ver 0.2.0
    * 출력 방식을 식당별에서 조,중,석식별 출력으로 변경
    * 괄호 안에 들어있는 쓸모없는 수식어구나 원산지 표기부분 제거
    * 출력방식 변경에 맞춰 필요없는 변수 제거

--- java/com/occidere/kookmincarteparser/MainActivity.java
+++ java/com/occidere/kookmincarteparser/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     static String print;
     private static final String address = "http://kmucoop.kookmin.ac.kr/restaurant/restaurant.php?w=";
-    private static int i;
+    private static String breakfast="", lunch="", dinner="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +34,12 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView)findViewById(R.id.mainText);
         try{
             String today = findToday();
-            bubsikPrint(today); print+="\n";
-            haksikPrint(today); print+="\n";
-            facultyPrint(today); print+="\n";
-            chunghyangPrint(today);
+            print+="## "+today+" 식단표 ##\n\n";
+            parseBubsik();
+            parseHaksik();
+            parseFaculty();
+            parseChunghyang();
+            printAll();
             tv.setText(print);
             tv.setTextColor(Color.BLACK);
         }
@@ -58,75 +60,98 @@ public class MainActivity extends AppCompatActivity {
         Document doc = Jsoup.connect(address).get();
         return doc.select(tag);
     }
-    //법식 출력
-    private static void bubsikPrint(String today) throws Exception {
-        print+="## 법학관 한울식당 메뉴 ("+today+") ##\n";
-        String tmp; i = 0;
-        String bubsikMenu[] = { "바로바로1", "바로바로2", "면이랑", "밥이랑 하나", "밥이랑 두울", "石火랑", "石火랑(조식)"};
-
+    //법식
+    private static void parseBubsik() throws Exception {
+        String bubsik = "[법식]", tmp;
         Elements menu = jsoupConnect(address+1);
-
         for (Element res : menu) {
-            tmp = res.text();
-            if (tmp.contains("*중식")) tmp = tmp.replace("*석식*", "\n*석식*");
-            bubsikMenu[i] = "------- <"+bubsikMenu[i]+"> -------\n" + tmp+"\n";
-
-            if(1<i && i<6) print+=bubsikMenu[i];
-            i++;
+            tmp = removeBracket(res.text());
+            if(tmp.contains("중식")) lunch+=bubsik+tmp.substring(tmp.indexOf("식*")+2)+"\n";
+            else if(tmp.contains("중석식")){
+                lunch+=bubsik+tmp.substring(tmp.indexOf("식*")+2)+"\n";
+                dinner+=bubsik+tmp.substring(tmp.indexOf("식*")+2)+"\n";
+            }
+            else breakfast+=bubsik+" "+tmp+"\n";
         }
+        breakfast+="\n"; lunch+="\n"; dinner+="\n";
     }
-    //학식 출력
-    private static void haksikPrint(String today) throws Exception {
-        print+="## 복지관 학생식당 메뉴 ("+today+") ##\n";
-        String haksikMenu[] = { "착한아침", "가마", "누들송(면)", "누들송(카페테리아)", "인터쉐프", "데일리밥"};
-
-        for(i=0;i<6;i++) haksikMenu[i] = "------- <"+haksikMenu[i]+"> -------\n"; i=0;
-
+    //학식
+    private static void parseHaksik() throws Exception {
+        String haksik = "[학식]", tmp;
         Elements menu = jsoupConnect(address+2);
-
+        int i=0;
         for (Element res : menu) {
-            String tmp = res.text();
+            tmp = removeBracket(res.text());
+            if(i==0) breakfast+=(haksik+" "+tmp+"\n"); //조식
+            else if(i<6) lunch+=(haksik+" "+tmp+"\n"); //중식
+            else dinner+=(haksik+" "+tmp+"\n"); //석식
+            i++;
+            if(i>8) break; //학식 중국집 메뉴는 출력 안한다.
+        }
+        breakfast+="\n"; lunch+="\n"; dinner+="\n";
+    }
 
-            if(i==0) tmp = "*조식* "+tmp+"\n";
-            else if(i<6) tmp = "*중식* "+tmp+"\n";
-            else tmp = "*석식* "+tmp+"\n";
-
-            if(i==6) haksikMenu[1]+=tmp;
-            else if(i==7) haksikMenu[4]+=tmp;
-            else if(i==8){ haksikMenu[5]+=tmp; break; }
-            else haksikMenu[i]+=tmp;
+    //교직원식당
+    private static void parseFaculty() throws Exception{
+        String faculty = "[교직원]", tmp;
+        Elements menu = jsoupConnect(address+3);
+        int i=0;
+        for(Element res : menu){
+            tmp = removeBracket(res.text());tmp = tmp.substring(tmp.indexOf(']')+1).trim();
+            if(i>2) dinner+=(faculty+" "+tmp+"\n");//석식
+            else lunch+=(faculty+" "+tmp+"\n");//중식
             i++;
         }
-        for(i=0;i<6;i++) print+=haksikMenu[i];
+        lunch+="\n"; dinner+="\n";
     }
-    //교직원식당 출력
-    private static void facultyPrint(String today) throws Exception{
-        print+="## 복지관 교직원식당 메뉴 ("+today+") ##\n";
-        String facultyMenu[] = {"키친1", "키친2", "주문식", "석식"};
 
-        for(i=0;i<4;i++) facultyMenu[i] = "------- <"+facultyMenu[i]+"> -------\n"; i=0;
-
-        Elements menu = jsoupConnect(address+3);
-
-        for(Element res : menu){
-            String tmp = res.text();
-
-            if(i>2) tmp = "*석식* "+tmp+"\n";
-            else tmp = "*중식* "+tmp+"\n";
-
-            print+=(facultyMenu[i++]+=tmp);
-        }
-    }
-    //청향 출력
-    private static void chunghyangPrint(String today) throws Exception{
-        print+="## 법학관 청향 메뉴 ("+today+") ##\n";
-        String chunghyangMenu[] = {"메뉴1", "메뉴2", "메뉴3", "메뉴4", "메뉴5", "메뉴6", "메뉴7"};
-
-        for(i=0;i<7;i++) chunghyangMenu[i] = "------- <"+chunghyangMenu[i]+"> -------\n"; i=0;
-
+    //청향
+    private static void parseChunghyang() throws Exception{
+        String chunghyang = "[청향]", tmp;
         Elements menu = jsoupConnect(address+4);
+        //청향은 중식만 운영
+        for(Element res : menu){
+            tmp = removeBracket(res.text());
+            lunch+=(chunghyang+" "+tmp+"\n");
+        }
+        lunch+="\n";
+    }
 
-        for(Element res : menu) print+=(chunghyangMenu[i++]+"*중식* "+res.text()+"\n");
+    //파싱한 메뉴 출력
+    private static void printAll(){
+        print+="------- <조식> -------\n";
+        print+=breakfast;
+        print+="------- <중식> -------\n";
+        print+=lunch;
+        print+="------- <석식> -------\n";
+        print+=dinner;
+    }
+
+    //메뉴 앞에 붙은 보기싫은 수식어구 제거
+    private static String removeBracket(String menu){
+        char tmp[] = menu.toCharArray();
+        int i, j=0, size = menu.length();
+        StringBuilder res = new StringBuilder();
+        char stack[] = new char[size];
+        for(i=0;i<size;i++){
+            //() 또는 []내부의 쓸모없는 원산지나 미사여구 제거
+            if(tmp[i]=='('){
+                while(true) {
+                    if(tmp[i]==')') break;
+                    else i++;
+                }
+            }
+            else if(tmp[i]=='[') {
+                while(true) {
+                    if(tmp[i]==']') break;
+                    else i++;
+                }
+            }
+            //괄호 밖의 평문인 경우 전부 스택에 담는다.
+            else stack[j++] = tmp[i];
+        }
+        for(i=0;i<j;i++) res.append(stack[i]);
+        return res.toString().trim();
     }
     //공유하기 기능
     public void onClick(View view){
